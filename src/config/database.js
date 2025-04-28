58349db44127c9c4acec4352690e24640742a26a
unknown <diegoiglesiasgodoy@gmail.com>
2025-04-28 11:56:39 -0400

    arreglo de contra

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 // Aquí debes poner tus datos correctos para la base de datos
-const sequelize = new Sequelize('railway', 'root', 'zRTdkHaCtfxRyykumXWmZvvixiOlpnOB$', {
+const sequelize = new Sequelize('railway', 'root', 'zRTdkHaCtfxRyykumXWmZvvixiOlpnOB', {
   host: 'yamabiko.proxy.rlwy.net', // O la dirección de tu base de datos
   dialect: 'mysql', // O el tipo de base de datos que estés usando ('postgres', 'sqlite', etc.)
   port: 51151,  // Asegúrate de que el puerto sea el correcto
