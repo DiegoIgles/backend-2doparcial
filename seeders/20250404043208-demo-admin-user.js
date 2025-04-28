ce9c48d800d78d42e9932f8bdccbfcaa0591fbd7
unknown <diegoiglesiasgodoy@gmail.com>
2025-04-28 13:22:18 -0400

    arrel

--- seeders/20250404043208-demo-admin-user.js
+++ seeders/20250404043208-demo-admin-user.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const { User } = require('../models');
+const { User } = require('../src/models/user');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
