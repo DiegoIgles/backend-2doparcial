0332f02e60fc42b146cb6678a9e8d06cb90f80e4
unknown <diegoiglesiasgodoy@gmail.com>
2025-04-28 01:38:37 -0400

    Primer commit

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@
 const { Sequelize } = require('sequelize');
 
 // Aquí debes poner tus datos correctos para la base de datos
-const sequelize = new Sequelize('primer_parcial_sw1', 'root', '', {
-  host: 'localhost', // O la dirección de tu base de datos
+const sequelize = new Sequelize('cisistem_primer_parcial_sw1', 'cisistem_sistemas', 'sistemas2025$', {
+  host: 'cisistemasficct.com', // O la dirección de tu base de datos
   dialect: 'mysql', // O el tipo de base de datos que estés usando ('postgres', 'sqlite', etc.)
 });
 
