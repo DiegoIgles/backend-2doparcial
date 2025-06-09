29601c77cbce78b0c36571b703be2db3308b31f8
unknown <diegoiglesiasgodoy@gmail.com>
2025-06-09 01:53:31 -0400

    credenciales sw1 2d

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@
 const { Sequelize } = require('sequelize');
 
 // Aquí debes poner tus datos correctos para la base de datos
-const sequelize = new Sequelize('segundo_parcial_sw1', 'root', '', {
-  host: 'localhost', // O la dirección de tu base de datos
+const sequelize = new Sequelize('railway', 'root', 'TnDkGfokJSHUBciAonEdgDjqxdlZvpIg', {
+  host: 'turntable.proxy.rlwy.net', // O la dirección de tu base de datos
   dialect: 'mysql', // O el tipo de base de datos que estés usando ('postgres', 'sqlite', etc.)
-  port: 3306,  // Asegúrate de que el puerto sea el correcto
+  port: 20389,  // Asegúrate de que el puerto sea el correcto
   dialectOptions: {
     connectTimeout: 80000,  // Aumentar el tiempo de espera (80 segundos)
   },
